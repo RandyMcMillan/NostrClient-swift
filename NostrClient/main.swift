1bb81b5dbbf91bf92e956ff3295e35f2bde1a0c9
Honma Masaru <46467282+HonmaMasaru@users.noreply.github.com>
2023-03-04 14:25:21 +0900

    簡略化

--- NostrClient/main.swift
+++ NostrClient/main.swift
@@ -32,39 +32,23 @@ struct NostrSendMessage: ParsableCommand {
         let message = try ClientMessage.event(event).string()
         let relay = relay ?? defaultRelay
         print("message: \(message), relay: \(relay)")
-        Task { [self] in
-            do {
-                let message = try await send(message, relay: relay)
-                print("response: \(String(describing: message))")
-                NostrSendMessage.exit()
-            } catch {
+
+        // 送信
+        let webSocketTask = URLSession(configuration: .default).webSocketTask(with: URL(string: relay)!)
+        webSocketTask.resume()
+        webSocketTask.send(.string(message)) {
+            if let error = $0 {
                 fatalError(error.localizedDescription)
             }
         }
-    }
-
-    /// メッセージの送信
-    /// - Parameters:
-    ///   - message: メッセージ
-    ///   - relay: リレー
-    /// - Returns: 結果
-    private func send(_ message: String, relay: String) async throws -> URLSessionWebSocketTask.Message {
-        try await withCheckedThrowingContinuation { continuation in
-            let webSocketTask = URLSession(configuration: .default).webSocketTask(with: URL(string: relay)!)
-            webSocketTask.resume()
-            webSocketTask.send(.string(message)) {
-                if let error = $0 {
-                    continuation.resume(throwing: error)
-                }
-            }
-            webSocketTask.receive {
-                webSocketTask.cancel(with: .goingAway, reason: nil)
-                switch $0 {
-                case .failure(let error):
-                    continuation.resume(throwing: error)
-                case .success(let message):
-                    continuation.resume(returning: message)
-                }
+        webSocketTask.receive {
+            webSocketTask.cancel(with: .goingAway, reason: nil)
+            switch $0 {
+            case .failure(let error):
+                fatalError(error.localizedDescription)
+            case .success(let message):
+                print("response: \(message)")
+                NostrSendMessage.exit()
             }
         }
     }
